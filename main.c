59beb4093b8a45012fe1b393d991ad5d46a4771a
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2025-02-21 00:08:43 +0100

    Add json boilerplate

--- main.c
+++ main.c
@@ -1,6 +1,38 @@
-#include <stdio.h>
+#include <err.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+#include <kcgi.h>
+#include <kcgijson.h>
+
+int
+main(void)
+{
+    struct kreq r;
+    struct kjsonreq req;
+    const char *page = "index";
+
+    if (khttp_parse(&r, NULL, 0, &page, 1, 0) != KCGI_OK)
+        return EXIT_FAILURE;
+
+    if (pledge("stdio", NULL) == -1)
+        err(EXIT_FAILURE, "pledge");
+
+    khttp_head(&r, kresps[KRESP_STATUS],
+      "%s", khttps[KHTTP_200]);
+    khttp_head(&r, kresps[KRESP_CONTENT_TYPE],
+      "%s", kmimetypes[KMIME_APP_JSON]);
+    khttp_body(&r);
+    kjson_open(&req, &r);
+    kjson_obj_open(&req);
+    kjson_objp_open(&req,"foo");
+    kjson_putstringp(&req, "greeting", "hello, world");
+    kjson_putstringp(&req, "foobaring", "Foo bar");
+    kjson_obj_close(&req);
+    kjson_obj_close(&req);
+    kjson_close(&req);
+    khttp_free(&r);
+    return EXIT_SUCCESS;
 }
