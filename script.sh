4836ce671d3101bc4b90e56da44241fb5920e26d
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2025-02-21 00:12:51 +0100

    change name

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 cc `pkg-config --static --cflags kcgi-html kcgi-json sqlbox` -c -o cgi.o main.c
-cc -static -o cgi cgi.o `pkg-config --static --libs kcgi-html kcgi-json sqlbox`
+cc -static -o api cgi.o `pkg-config --static --libs kcgi-html kcgi-json sqlbox`
 [ -d /var/www/cgi-bin/mellow/db ] || doas mkdir /var/www/cgi-bin/mellow/db
 doas chown www:www /var/www/cgi-bin/mellow/db
 doas chmod 0700 /var/www/cgi-bin/mellow/db
-doas install -o www -g www -m 0500 cgi /var/www/cgi-bin/mellow
+doas install -o www -g www -m 0500 api /var/www/cgi-bin/mellow
 rm -rfv db.db
 sqlite3 db.db < database-scheme.sql
 doas install -o www -g www -m 0600 db.db /var/www/cgi-bin/mellow/db
