c837f3a0e252a485eb131560003e18a3c4008b17
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2025-02-21 15:10:39 +0100

    Rename database

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ cc -static -o api cgi.o `pkg-config --static --libs kcgi-html kcgi-json sqlbox`
 doas chown www:www /var/www/cgi-bin/mellow/db
 doas chmod 0700 /var/www/cgi-bin/mellow/db
 doas install -o www -g www -m 0500 api /var/www/cgi-bin/mellow
-rm -rfv db.db
-sqlite3 db.db < database-scheme.sql
-doas install -o www -g www -m 0600 db.db /var/www/cgi-bin/mellow/db
+rm -rfv database.db
+sqlite3 database.db < database-scheme.sql
+doas install -o www -g www -m 0600 database.db /var/www/cgi-bin/mellow/db
 doas rcctl enable slowcgi
 doas rcctl start slowcgi
 doas rcctl check slowcgi
