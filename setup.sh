ce20e66bcd2c490fd4992a67859c4fc4260e2d04
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2025-02-23 23:01:27 +0100

    Microservices

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
-cc `pkg-config --static --cflags kcgi-html kcgi-json sqlbox` -c -o cgi.o main.c
-cc -static -o api cgi.o `pkg-config --static --libs kcgi-html kcgi-json sqlbox`
+cc `pkg-config --static --cflags kcgi-html kcgi-json sqlbox` -c -o query.o src/query.c
+cc -static -o bins/query query.o `pkg-config --static --libs kcgi-html kcgi-json sqlbox`
 [ -d /var/www/cgi-bin/mellow ] || doas mkdir /var/www/cgi-bin/mellow
 [ -d /var/www/cgi-bin/mellow/db ] || doas mkdir /var/www/cgi-bin/mellow/db
 doas chown www:www /var/www/cgi-bin/mellow/db
 doas chmod 0700 /var/www/cgi-bin/mellow/db
-doas install -o www -g www -m 0500 api /var/www/cgi-bin/mellow
+doas install -o www -g www -m 0500 bins/query /var/www/cgi-bin/mellow
 rm -rfv database.db
 sqlite3 database.db < database-scheme.sql
 doas install -o www -g www -m 0600 database.db /var/www/cgi-bin/mellow/db
