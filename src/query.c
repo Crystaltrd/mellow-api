6bf40b886c45f142ea23a4816e43f379355bf185
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2025-02-23 23:34:04 +0100

    SQLite

--- src/query.c
+++ src/query.c
@@ -22,6 +22,21 @@ static const char *pages[PG__MAX] = {
 };
 
 void handle_err(struct kreq *r, struct kjsonreq *req, enum khttp status, int err) {
+
+    khttp_head(r, kresps[KRESP_STATUS], "%s", khttps[status]);
+    khttp_head(r, kresps[KRESP_CONTENT_TYPE],
+               "%s", kmimetypes[KMIME_APP_JSON]);
+    khttp_body(r);
+    kjson_open(req, r);
+    kjson_obj_open(req);
+    kjson_putintp(req, "status", err);
+
+    kjson_obj_close(req);
+    kjson_close(req);
+    khttp_free(r);
+}
+
+void handle_campuses(struct kreq *r, struct kjsonreq *req) {
     size_t dbid, stmtid;
     struct sqlbox *p2;
     struct sqlbox_cfg cfg;
@@ -32,7 +47,7 @@ void handle_err(struct kreq *r, struct kjsonreq *req, enum khttp status, int err
         }
     };
     struct sqlbox_pstmt pstmts[] = {
-        {.stmt = (char *) "SELECT (ROWID,CAMPUS_NAME) FROM CAMPUSES"},
+        {.stmt = (char *) "SELECT ROWID,* FROM CAMPUSES"},
     };
     const struct sqlbox_parmset *res;
 
@@ -48,13 +63,13 @@ void handle_err(struct kreq *r, struct kjsonreq *req, enum khttp status, int err
         errx(EXIT_FAILURE, "sqlbox_open");
     if (!(stmtid = sqlbox_prepare_bind(p2, dbid, 0, 0, 0, 0)))
         errx(EXIT_FAILURE, "sqlbox_prepare_bind");
-    khttp_head(r, kresps[KRESP_STATUS], "%s", khttps[status]);
+    khttp_head(r, kresps[KRESP_STATUS], "%s", khttps[KHTTP_200]);
     khttp_head(r, kresps[KRESP_CONTENT_TYPE],
                "%s", kmimetypes[KMIME_APP_JSON]);
     khttp_body(r);
     kjson_open(req, r);
     kjson_obj_open(req);
-    kjson_putintp(req, "status", err);
+    kjson_putintp(req, "status", 200);
     kjson_arrayp_open(req, "campus");
     while ((res = sqlbox_step(p2, stmtid)) != NULL && res->code == SQLBOX_CODE_OK && res->psz != 0) {
         kjson_obj_open(req);
@@ -65,20 +80,6 @@ void handle_err(struct kreq *r, struct kjsonreq *req, enum khttp status, int err
     if (!sqlbox_finalise(p2, stmtid))
         errx(EXIT_FAILURE, "sqlbox_finalise");
     sqlbox_free(p2);
-    kjson_obj_close(req);
-    kjson_close(req);
-    khttp_free(r);
-}
-
-void handle_campuses(struct kreq *r, struct kjsonreq *req) {
-    khttp_head(r, kresps[KRESP_STATUS], "%s", khttps[KHTTP_200]);
-    khttp_head(r, kresps[KRESP_CONTENT_TYPE],
-               "%s", kmimetypes[KMIME_APP_JSON]);
-    khttp_body(r);
-    kjson_open(req, r);
-    kjson_obj_open(req);
-    kjson_putintp(req, "status", 200);
-    kjson_putstringp(req, "answer", "campus");
     kjson_obj_close(req);
     kjson_close(req);
     khttp_free(r);
