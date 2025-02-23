2f0ac2b0efba5294e14aab20d1bc0efcd6c22393
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2025-02-23 23:18:24 +0100

    Add campus micro service

--- src/query.c
+++ src/query.c
@@ -2,20 +2,66 @@
 #include <stdarg.h> /* va_list */
 #include <stddef.h> /* NULL */
 #include <stdint.h> /* int64_t */
+#include <unistd.h> /* pledge() */
+#include <err.h> /* err(), warnx() */
+#include <stdlib.h> /* EXIT_FAILURE */
+#include <string.h> /* memset() */
 #include <kcgi.h>
+#include <kcgijson.h>
+#include <sqlbox.h>
 
-int main(void) {
-    struct kreq r;
-    const char *page = "index";
-    if (khttp_parse(&r, NULL, 0, &page, 1, 0) != KCGI_OK)
-        return 1;
-    khttp_head(&r, kresps[KRESP_STATUS],
-        "%s", khttps[KHTTP_200]);
-    khttp_head(&r, kresps[KRESP_CONTENT_TYPE],
-        "%s", kmimetypes[KMIME_TEXT_PLAIN]);
-    khttp_body(&r);
-    khttp_puts(&r, "Hello, world!");
-    khttp_free(&r);
-    return 0;
+enum pg {
+    PG_BOOKS,
+    PG_CAMPUSES,
+    PG__MAX
+};
+
+static const char *pages[PG__MAX] = {
+    "book",
+    "campus",
+};
+
+void handle_err(struct kreq *r, struct kjsonreq *req, enum khttp status) {
+    khttp_head(r, kresps[KRESP_STATUS], "%s", khttps[status]);
+    khttp_head(r, kresps[KRESP_CONTENT_TYPE],
+               "%s", kmimetypes[KMIME_APP_JSON]);
+    khttp_body(r);
+    kjson_open(req, r);
+    kjson_obj_open(req);
+    kjson_putstringp(req, "error", khttps[status]);
+    kjson_obj_close(req);
+    kjson_close(req);
+    khttp_free(r);
 }
 
+void handle_campuses(struct kreq *r, struct kjsonreq *req) {
+     khttp_head(r, kresps[KRESP_STATUS], "%s", khttps[KHTTP_200]);
+    khttp_head(r, kresps[KRESP_CONTENT_TYPE],
+               "%s", kmimetypes[KMIME_APP_JSON]);
+    khttp_body(r);
+    kjson_open(req, r);
+    kjson_obj_open(req);
+    kjson_putstringp(req,"answer","campus");
+    kjson_obj_close(req);
+    kjson_close(req);
+    khttp_free(r);
+}
+int main(void) {
+    struct kreq r;
+    struct kjsonreq req;
+    if (khttp_parse(&r, NULL, 0, pages, PG__MAX, PG_BOOKS) != KCGI_OK)
+        return EXIT_FAILURE;
+    if (r.method != KMETHOD_GET && r.method != KMETHOD_HEAD) {
+        handle_err(&r, &req, KHTTP_405);
+    } else {
+        switch (r.page) {
+            case PG_CAMPUSES:
+                handle_campuses(&r,&req);
+                break;
+            default:
+                handle_err(&r, &req, KHTTP_403);
+                break;
+        }
+    }
+    return EXIT_SUCCESS;
+}
