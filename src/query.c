6c865f89d294a293ad34775ac45ba504572d227d
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2025-02-23 23:31:02 +0100

    SQLite

--- src/query.c
+++ src/query.c
@@ -21,7 +21,33 @@ static const char *pages[PG__MAX] = {
     "campus",
 };
 
-void handle_err(struct kreq *r, struct kjsonreq *req, enum khttp status,int err) {
+void handle_err(struct kreq *r, struct kjsonreq *req, enum khttp status, int err) {
+    size_t dbid, stmtid;
+    struct sqlbox *p2;
+    struct sqlbox_cfg cfg;
+    struct sqlbox_src srcs[] = {
+        {
+            .fname = (char *) "db/database.db",
+            .mode = SQLBOX_SRC_RWC
+        }
+    };
+    struct sqlbox_pstmt pstmts[] = {
+        {.stmt = (char *) "SELECT (ROWID,CAMPUS_NAME) FROM CAMPUSES"},
+    };
+    const struct sqlbox_parmset *res;
+
+    memset(&cfg, 0, sizeof(struct sqlbox_cfg));
+    cfg.msg.func_short = warnx;
+    cfg.srcs.srcsz = 1;
+    cfg.srcs.srcs = srcs;
+    cfg.stmts.stmtsz = 1;
+    cfg.stmts.stmts = pstmts;
+    if ((p2 = sqlbox_alloc(&cfg)) == NULL)
+        errx(EXIT_FAILURE, "sqlbox_alloc");
+    if (!(dbid = sqlbox_open(p2, 0)))
+        errx(EXIT_FAILURE, "sqlbox_open");
+    if (!(stmtid = sqlbox_prepare_bind(p2, dbid, 0, 0, 0, 0)))
+        errx(EXIT_FAILURE, "sqlbox_prepare_bind");
     khttp_head(r, kresps[KRESP_STATUS], "%s", khttps[status]);
     khttp_head(r, kresps[KRESP_CONTENT_TYPE],
                "%s", kmimetypes[KMIME_APP_JSON]);
@@ -29,38 +55,49 @@ void handle_err(struct kreq *r, struct kjsonreq *req, enum khttp status,int err)
     kjson_open(req, r);
     kjson_obj_open(req);
     kjson_putintp(req, "status", err);
+    kjson_arrayp_open(req, "campus");
+    while ((res = sqlbox_step(p2, stmtid)) != NULL && res->code == SQLBOX_CODE_OK && res->psz != 0) {
+        kjson_obj_open(req);
+        kjson_putintp(req, "rowid", res->ps[0].iparm);
+        kjson_putstringp(req, "campus", res->ps[1].sparm);
+        kjson_obj_close(req);
+    }
+    if (!sqlbox_finalise(p2, stmtid))
+        errx(EXIT_FAILURE, "sqlbox_finalise");
+    sqlbox_free(p2);
     kjson_obj_close(req);
     kjson_close(req);
     khttp_free(r);
 }
 
 void handle_campuses(struct kreq *r, struct kjsonreq *req) {
-     khttp_head(r, kresps[KRESP_STATUS], "%s", khttps[KHTTP_200]);
+    khttp_head(r, kresps[KRESP_STATUS], "%s", khttps[KHTTP_200]);
     khttp_head(r, kresps[KRESP_CONTENT_TYPE],
                "%s", kmimetypes[KMIME_APP_JSON]);
     khttp_body(r);
     kjson_open(req, r);
     kjson_obj_open(req);
     kjson_putintp(req, "status", 200);
-    kjson_putstringp(req,"answer","campus");
+    kjson_putstringp(req, "answer", "campus");
     kjson_obj_close(req);
     kjson_close(req);
     khttp_free(r);
 }
+
 int main(void) {
     struct kreq r;
     struct kjsonreq req;
     if (khttp_parse(&r, NULL, 0, pages, PG__MAX, PG_BOOKS) != KCGI_OK)
         return EXIT_FAILURE;
     if (r.method != KMETHOD_GET && r.method != KMETHOD_HEAD) {
-        handle_err(&r, &req, KHTTP_405,405);
+        handle_err(&r, &req, KHTTP_405, 405);
     } else {
         switch (r.page) {
             case PG_CAMPUSES:
-                handle_campuses(&r,&req);
+                handle_campuses(&r, &req);
                 break;
             default:
-                handle_err(&r, &req, KHTTP_403,403);
+                handle_err(&r, &req, KHTTP_403, 403);
                 break;
         }
     }
