a9cb200c3e1eb651f23576fc0226b961543329d7
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2025-03-01 22:54:11 +0100

    Add category

--- src/query.c
+++ src/query.c
@@ -287,7 +287,97 @@ void handle_simple(struct kreq *r, struct kjsonreq *req, const int rowid) {
     kjson_close(req);
     khttp_free(r);
 }
+void handle_category(struct kreq *r,struct kjsonreq *req,const int rowid) {
+    size_t dbid, stmtid;
+    struct sqlbox *p2;
+    struct sqlbox_cfg cfg;
+    struct sqlbox_src srcs[] = {
+        {
+            .fname = (char *) "db/database.db",
+            .mode = SQLBOX_SRC_RO
+        }
+    };
+    struct sqlbox_pstmt pstmts[]={
+        {.stmt = (char *) "SELECT ROWID,categoryName,categoryDesc FROM CATEGORY"},
+        {.stmt = (char *) "SELECT categoryName,categoryDesc FROM CATEGORY WHERE ROWID = (?)"},
+    };
 
+    struct sqlbox_parm parms[] = {
+        {
+            .iparm = rowid,
+            .type = SQLBOX_PARM_INT
+        },
+    };
+    const struct sqlbox_parmset *res;
+
+    memset(&cfg, 0, sizeof(struct sqlbox_cfg));
+    cfg.msg.func_short = warnx;
+    cfg.srcs.srcsz = 1;
+    cfg.srcs.srcs = srcs;
+    cfg.stmts.stmtsz = 2;
+    cfg.stmts.stmts = pstmts;
+    if ((p2 = sqlbox_alloc(&cfg)) == NULL)
+        errx(EXIT_FAILURE, "sqlbox_alloc");
+    if (!(dbid = sqlbox_open(p2, 0)))
+        errx(EXIT_FAILURE, "sqlbox_open");
+    if (rowid > 0) {
+        if (!(stmtid = sqlbox_prepare_bind(p2, dbid, 0, 1, parms, 0)))
+            errx(EXIT_FAILURE, "sqlbox_prepare_bind");
+    } else {
+        if (!(stmtid = sqlbox_prepare_bind(p2, dbid, 1, 0, 0, 0)))
+            errx(EXIT_FAILURE, "sqlbox_prepare_bind");
+    }
+
+    if (rowid <= 0) {
+        khttp_head(r, kresps[KRESP_STATUS], "%s", khttps[KHTTP_200]);
+        khttp_head(r, kresps[KRESP_CONTENT_TYPE],
+                   "%s", kmimetypes[KMIME_APP_JSON]);
+        khttp_head(r, kresps[KRESP_ACCESS_CONTROL_ALLOW_ORIGIN], "%s", "*");
+        khttp_head(r, kresps[KRESP_VARY], "%s", "Origin");
+        khttp_body(r);
+        kjson_open(req, r);
+        kjson_obj_open(req);
+        kjson_arrayp_open(req, "category");
+        while ((res = sqlbox_step(p2, stmtid)) != NULL && res->code == SQLBOX_CODE_OK && res->psz != 0) {
+            kjson_obj_open(req);
+            kjson_putintp(req,"rowid",res->ps[0].iparm);
+            kjson_putstringp(req,"categoryName",res->ps[1].sparm);
+            kjson_putstringp(req,"categoryDesc",res->ps[2].sparm);
+            kjson_obj_close(req);
+        }
+        kjson_array_close(req);
+        kjson_putintp(req, "status", 200);
+    } else {
+        if ((res = sqlbox_step(p2, stmtid)) == NULL)
+            errx(EXIT_FAILURE, "sqlbox_step");
+        if (res->psz != 0) {
+            khttp_head(r, kresps[KRESP_STATUS], "%s", khttps[KHTTP_200]);
+            khttp_head(r, kresps[KRESP_CONTENT_TYPE], "%s", kmimetypes[KMIME_APP_JSON]);
+            khttp_head(r, kresps[KRESP_ACCESS_CONTROL_ALLOW_ORIGIN], "%s", "*");
+            khttp_head(r, kresps[KRESP_VARY], "%s", "Origin");
+            khttp_body(r);
+            kjson_open(req, r);
+            kjson_obj_open(req);
+            kjson_putstringp(req,"categoryName",res->ps[0].sparm);
+            kjson_putstringp(req,"categoryDesc",res->ps[1].sparm);
+            kjson_putintp(req, "status", 200);
+        } else {
+            khttp_head(r, kresps[KRESP_STATUS], "%s", khttps[KHTTP_404]);
+            khttp_head(r, kresps[KRESP_CONTENT_TYPE],
+                       "%s", kmimetypes[KMIME_APP_JSON]);
+            khttp_body(r);
+            kjson_open(req, r);
+            kjson_obj_open(req);
+            kjson_putintp(req, "status", 404);
+        }
+    }
+    if (!sqlbox_finalise(p2, stmtid))
+        errx(EXIT_FAILURE, "sqlbox_finalise");
+    sqlbox_free(p2);
+    kjson_obj_close(req);
+    kjson_close(req);
+    khttp_free(r);
+}
 int main(void) {
     struct kreq r;
     struct kjsonreq req;
@@ -304,6 +394,14 @@ int main(void) {
             case PG_DOCTYPE:
             case PG_CAMPUS:
             case PG_ROLE:
+                if ((rowid = r.fieldmap[KEY_ROWID]))
+                    handle_simple(&r, &req, (int) rowid->parsed.i);
+                else if (r.fieldnmap[KEY_ROWID])
+                    handle_err(&r, &req, KHTTP_400, 400);
+                else
+                    handle_simple(&r, &req, -1);
+                break;
+            case PG_CATEGORY:
                 if ((rowid = r.fieldmap[KEY_ROWID]))
                     handle_simple(&r, &req, (int) rowid->parsed.i);
                 else if (r.fieldnmap[KEY_ROWID])
