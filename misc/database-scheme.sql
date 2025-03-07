01bb870e3619dc2aee279c51b4dfdfd53bcd7593
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2025-03-07 22:50:43 +0100

    Dynamic stuff
    Signed-off-by: Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>

--- misc/database-scheme.sql
+++ misc/database-scheme.sql
@@ -109,65 +109,112 @@ VALUES ('ADMIN', 64),
 
 INSERT INTO CATEGORY
 VALUES ('0 Science and knowledge. Organization. Computer science. Information. Documentation. Librarianship. Institution. Publications',
-        null),
-       ('1 Philosophy. Psychology', null),
-       ('2 Religion. Theology', null),
-       ('3 Social sciences', null),
-       ('4 vacant', null),
-       ('5 Mathematics. Natural Sciences', null),
-       ('6 Applied Sciences. Medicine, Technology', null),
-       ('7 The Arts. Entertainment. Sport', null),
-       ('8 Linguistics. Literature', null),
-       ('9 Geography. History', null),
-       ('00 Prolegomena. Fundamentals of knowledge and culture. Propaedeutics',
-        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '0 %')),
-       ('001 Science and knowledge in general. Organization of intellectual work',
-        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '00 %')),
-       ('002 Documentation. Books. Writings. Authorship', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '00 %')),
-       ('003 Writing systems and scripts', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '00 %')),
-       ('004 Computer science and technology. Computing. Data processing',
-        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '00 %')),
-       ('004.2 Computer architecture', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '004 %')),
-       ('004.3 Computer hardware', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '004 %')),
-       ('004.4 Software', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '004 %')),
-       ('004.5 Human-computer interaction. Man-machine interface. User interface. User environment',
-        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '004 %')),
-       ('004.6 Data', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '004 %')),
-       ('004.7 Computer communication', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '004 %')),
-       ('004.8 Artificial intelligence', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '004 %')),
-       ('004.9 Application-oriented computer-based techniques',
-        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '004 %')),
-       ('005 Management', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '00 %')),
-       ('005.1 Management Theory', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005 %')),
-       ('005.2 Management agents. Mechanisms. Measures', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005 %')),
-       ('005.3 Management activities', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005 %')),
-       ('005.4 Processes in management', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005 %')),
-       ('005.5 Management operations. Direction', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005 %')),
-       ('005.6 Quality management. Total quality management (TQM)',
-        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005 %')),
-       ('005.7 Organizational management (OM)', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005 %')),
-       ('005.9 Fields of management', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005 %')),
-       ('005.91 Administrative management. Secretariat',
-        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005.9 %')),
-       ('005.92 Records management', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005.9 %')),
-       ('005.93 Plant management. Physical resources management',
-        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005.9 %')),
-       ('005.94 Knowledge management', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005.9 %')),
-       ('005.95/96 Personnel management. Human Resources management',
-        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005.9 %')),
-       ('006 Standardization of products, operations, weights, measures and time',
-        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '00 %')),
-       ('007 Activity and organizing. Information. Communication and control theory generally (cybernetics)',
-        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '00 %')),
-       ('008 Civilization. Culture. Progress', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '00 %')),
-       ('01 Bibliography and bibliographies. Catalogues', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '0 %')),
-       ('02 Librarianship', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '0 %')),
-       ('030 General reference works (as subject)', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '0 %')),
-       ('050 Serial publications, periodicals (as subject)',
-        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '0 %')),
-       ('06 Organizations of a general nature', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '0 %')),
-       ('070 Newspapers (as subject). The Press. Outline of journalism',
-        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '0 %')),
-       ('08 Polygraphies. Collective works (as subject)', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '0 %')),
-       ('09 Manuscripts. Rare and remarkable works (as subject)',
+        null);
+INSERT INTO CATEGORY
+VALUES ('1 Philosophy. Psychology', null);
+INSERT INTO CATEGORY
+VALUES ('2 Religion. Theology', null);
+INSERT INTO CATEGORY
+VALUES ('3 Social sciences', null);
+INSERT INTO CATEGORY
+VALUES ('4 vacant', null);
+INSERT INTO CATEGORY
+VALUES ('5 Mathematics. Natural Sciences', null);
+INSERT INTO CATEGORY
+VALUES ('6 Applied Sciences. Medicine, Technology', null);
+INSERT INTO CATEGORY
+VALUES ('7 The Arts. Entertainment. Sport', null);
+INSERT INTO CATEGORY
+VALUES ('8 Linguistics. Literature', null);
+INSERT INTO CATEGORY
+VALUES ('9 Geography. History', null);
+INSERT INTO CATEGORY
+VALUES ('00 Prolegomena. Fundamentals of knowledge and culture. Propaedeutics',
+        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '0 %'));
+INSERT INTO CATEGORY
+VALUES ('001 Science and knowledge in general. Organization of intellectual work',
+        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '00 %'));
+INSERT INTO CATEGORY
+VALUES ('002 Documentation. Books. Writings. Authorship', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '00 %'));
+INSERT INTO CATEGORY
+VALUES ('003 Writing systems and scripts', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '00 %'));
+INSERT INTO CATEGORY
+VALUES ('004 Computer science and technology. Computing. Data processing',
+        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '00 %'));
+INSERT INTO CATEGORY
+VALUES ('004.2 Computer architecture', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '004 %'));
+INSERT INTO CATEGORY
+VALUES ('004.3 Computer hardware', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '004 %'));
+INSERT INTO CATEGORY
+VALUES ('004.4 Software', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '004 %'));
+INSERT INTO CATEGORY
+VALUES ('004.5 Human-computer interaction. Man-machine interface. User interface. User environment',
+        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '004 %'));
+INSERT INTO CATEGORY
+VALUES ('004.6 Data', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '004 %'));
+INSERT INTO CATEGORY
+VALUES ('004.7 Computer communication', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '004 %'));
+INSERT INTO CATEGORY
+VALUES ('004.8 Artificial intelligence', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '004 %'));
+INSERT INTO CATEGORY
+VALUES ('004.9 Application-oriented computer-based techniques',
+        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '004 %'));
+INSERT INTO CATEGORY
+VALUES ('005 Management', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '00 %'));
+INSERT INTO CATEGORY
+VALUES ('005.1 Management Theory', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005 %'));
+INSERT INTO CATEGORY
+VALUES ('005.2 Management agents. Mechanisms. Measures', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005 %'));
+INSERT INTO CATEGORY
+VALUES ('005.3 Management activities', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005 %'));
+INSERT INTO CATEGORY
+VALUES ('005.4 Processes in management', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005 %'));
+INSERT INTO CATEGORY
+VALUES ('005.5 Management operations. Direction', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005 %'));
+INSERT INTO CATEGORY
+VALUES ('005.6 Quality management. Total quality management (TQM)',
+        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005 %'));
+INSERT INTO CATEGORY
+VALUES ('005.7 Organizational management (OM)', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005 %'));
+INSERT INTO CATEGORY
+VALUES ('005.9 Fields of management', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005 %'));
+INSERT INTO CATEGORY
+VALUES ('005.91 Administrative management. Secretariat',
+        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005.9 %'));
+INSERT INTO CATEGORY
+VALUES ('005.92 Records management', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005.9 %'));
+INSERT INTO CATEGORY
+VALUES ('005.93 Plant management. Physical resources management',
+        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005.9 %'));
+INSERT INTO CATEGORY
+VALUES ('005.94 Knowledge management', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005.9 %'));
+INSERT INTO CATEGORY
+VALUES ('005.95/96 Personnel management. Human Resources management',
+        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '005.9 %'));
+INSERT INTO CATEGORY
+VALUES ('006 Standardization of products, operations, weights, measures and time',
+        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '00 %'));
+INSERT INTO CATEGORY
+VALUES ('007 Activity and organizing. Information. Communication and control theory generally (cybernetics)',
+        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '00 %'));
+INSERT INTO CATEGORY
+VALUES ('008 Civilization. Culture. Progress', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '00 %'));
+INSERT INTO CATEGORY
+VALUES ('01 Bibliography and bibliographies. Catalogues', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '0 %'));
+INSERT INTO CATEGORY
+VALUES ('02 Librarianship', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '0 %'));
+INSERT INTO CATEGORY
+VALUES ('030 General reference works (as subject)', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '0 %'));
+INSERT INTO CATEGORY
+VALUES ('050 Serial publications, periodicals (as subject)',
+        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '0 %'));
+INSERT INTO CATEGORY
+VALUES ('06 Organizations of a general nature', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '0 %'));
+INSERT INTO CATEGORY
+VALUES ('070 Newspapers (as subject). The Press. Outline of journalism',
+        (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '0 %'));
+INSERT INTO CATEGORY
+VALUES ('08 Polygraphies. Collective works (as subject)', (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '0 %'));
+INSERT INTO CATEGORY
+VALUES ('09 Manuscripts. Rare and remarkable works (as subject)',
         (SELECT ROWID FROM CATEGORY WHERE categoryName LIKE '0 %'));
