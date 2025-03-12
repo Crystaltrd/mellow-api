b32d8e545e73770f4838462a0728cd69ac835802
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2025-03-12 23:41:28 +0100

    Working on the db
    Signed-off-by: Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>

--- misc/database-scheme.sql
+++ misc/database-scheme.sql
@@ -19,13 +19,6 @@ CREATE TABLE DOCTYPE
 (
     typeName TEXT PRIMARY KEY
 );
-CREATE TABLE CATEGORY
-(
-    categoryClass    TEXT PRIMARY KEY,
-    categoryName     TEXT UNIQUE NOT NULL,
-    parentCategoryID TEXT,
-    FOREIGN KEY (parentCategoryID) REFERENCES CATEGORY (categoryClass) ON UPDATE CASCADE ON DELETE CASCADE
-);
 CREATE TABLE CAMPUS
 (
     campusName TEXT PRIMARY KEY
@@ -34,6 +27,14 @@ CREATE TABLE ROLE
 (
     roleName TEXT PRIMARY KEY,
     perms    INTEGER NOT NULL
+);
+
+CREATE TABLE CATEGORY
+(
+    categoryClass    TEXT PRIMARY KEY,
+    categoryName     TEXT UNIQUE NOT NULL,
+    parentCategoryID TEXT,
+    FOREIGN KEY (parentCategoryID) REFERENCES CATEGORY (categoryClass) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
@@ -55,7 +56,7 @@ CREATE TABLE BOOK
     publisher TEXT NOT NULL,
     booktitle       TEXT                NOT NULL,
     bookreleaseyear INTEGER             NOT NULL,
-    bookcover       TEXT                NOT NULL,
+    bookcover TEXT,
     hits      INTEGER DEFAULT 0,
     FOREIGN KEY (type) REFERENCES DOCTYPE (typeName) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (category) REFERENCES CATEGORY (categoryClass) ON UPDATE CASCADE ON DELETE CASCADE,
@@ -121,16 +122,39 @@ VALUES ('ADMIN', 64),
        ('LIBRARIAN', 6),
        ('PROFESSOR', 1),
        ('STUDENT', 1);
+INSERT INTO CATEGORY
+VALUES ('00', 'Category', null);
+INSERT INTO DOCTYPE
+VALUES ('type');
+INSERT INTO PUBLISHER
+VALUES ('Pub1'),
+       ('Pub2'),
+       ('Pub3');
+
+INSERT INTO BOOK
+VALUES (123, 'type', '00', 'Pub1', 'Hello', 2005, 'hello', 99),
+       (12333, 'type', '00', 'Pub2', 'Hello', 2005, 'hello', 1),
+       (14423, 'type', '00', 'Pub2', 'Hello', 2005, 'hello', 1);
+
+INSERT INTO AUTHOR
+VALUES ('AUTHOR1'),
+       ('AUTHOR2'),
+       ('AUTHOR3');
+
+INSERT INTO AUTHORED
+VALUES ('AUTHOR1', 123),
+       ('AUTHOR1', 12333),
+       ('AUTHOR2', 14423);
 
 /*
 WITH RECURSIVE CategoryCascade AS (SELECT ROWID, parentCategoryID
-                                FROM CATEGORY
-                                WHERE ROWID = 3
-                                UNION ALL
-                                SELECT c.ROWID, c.parentCategoryID
-                                FROM CATEGORY c
-                                         INNER JOIN CategoryCascade ct ON c.parentCategoryID = ct.ROWID)
+                            FROM CATEGORY
+                            WHERE ROWID = 3
+                            UNION ALL
+                            SELECT c.ROWID, c.parentCategoryID
+                            FROM CATEGORY c
+                                     INNER JOIN CategoryCascade ct ON c.parentCategoryID = ct.ROWID)
 SELECT CATEGORY.ROWID, CATEGORY.categoryName
 FROM CATEGORY,
-     CategoryCascade
+ CategoryCascade
 WHERE CategoryCascade.ROWID = CATEGORY.ROWID;*/
