87596a2c8456f77f6d39e889a0be064784029b42
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2025-03-21 16:45:54 +0100

    break down
    Signed-off-by: Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>

--- src/query.c
+++ src/query.c
@@ -286,7 +286,7 @@ static struct sqlbox_pstmt pstmts[STMTS__MAX] = {
     },
     {
 
-        (char *)"SELECT * FROM HISTORY LIMIT 10 OFFSET (? * 10)"},
+        (char *)"SELECT * FROM HISTORY ORDER BY actiondate DESC LIMIT 10 OFFSET (? * 10)"},
 
 };
 struct sqlbox_parm *parms; //Array of statement parameters
