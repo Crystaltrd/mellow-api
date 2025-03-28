d641962780e49ca421c97c657b434a5ab2d37116
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2025-03-28 18:49:31 +0100

    Revert "Put inside object"
    This reverts commit e8947a8dd729a240bf0c527dfe9cc6b8c8b6d763.

--- src/query.c
+++ src/query.c
@@ -1159,7 +1159,7 @@ void process(const enum statement STATEMENT) {
     khttp_body(&r);
     kjson_open(&req, &r);
     kjson_obj_open(&req);
-    kjson_arrayp_open(&req,"res");
+    kjson_array_open(&req);
     while ((res = sqlbox_step(boxctx, stmtid)) != NULL && res->code == SQLBOX_CODE_OK && res->psz != 0) {
         kjson_obj_open(&req);
         for (int i = 0; i < (int) res->psz; ++i) {
