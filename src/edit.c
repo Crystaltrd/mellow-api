5931336015521647d19ef4505bd305136464d066
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2025-04-07 22:40:54 +0100

    Test page thing

--- src/edit.c
+++ src/edit.c
@@ -56,6 +56,28 @@ enum key {
     KEY_PG_INVENTORY,
     KEY_PG_HISTORY,
     KEY_PG_SESSIONS,
+    KEY_NAME,
+    KEY_PERMS,
+    KEY_CLASS,
+    KEY_PARENTID,
+    KEY_UUID,
+    KEY_PASSWORD,
+    KEY_CAMPUS,
+    KEY_ROLE,
+    KEY_FROZEN,
+    KEY_SERIALNUM,
+    KEY_TYPE,
+    KEY_CATEGORY,
+    KEY_PUBLISHER,
+    KEY_YEAR,
+    KEY_COVER,
+    KEY_HITS,
+    KEY_LANG,
+    KEY_AUTHOR,
+    KEY_STOCK,
+    KEY_DURATION,
+    KEY_DATE,
+    KEY_EXTENDED,
     KEY__MAX
 };
 
@@ -97,7 +119,6 @@ static const char *statement_string[STMTS__MAX] = {
     "publisher", "author", "lang", "action", "type", "campus", "role", "category", "account",
     "book", "booklang", "bookauthor", "stock", "inventory"
 };
-
 static struct sqlbox_pstmt pstmts_top[STMTS__MAX] = {
     {(char *) "UPDATE PUBLISHER SET "},
     {(char *) "UPDATE AUTHOR SET "},
@@ -120,7 +141,8 @@ static struct sqlbox_pstmt pstmts_top[STMTS__MAX] = {
     }
 };
 
-static struct sqlbox_pstmt pstmts_middleware[STMTS__MAX - 1][7] = {
+
+static struct sqlbox_pstmt pstmts_switches[STMTS__MAX - 1][8] = {
     {{(char *) "publisherName = (?) "}},
     {{(char *) "authorName = (?) "}},
     {{(char *) "actionName = (?) "}},
@@ -134,9 +156,9 @@ static struct sqlbox_pstmt pstmts_middleware[STMTS__MAX - 1][7] = {
         {(char *) "campus = (?) "}, {(char *) "role = (?) "}, {(char *) "frozen = (?) "}
     },
     {
-        {(char *) "type = (?) "}, {(char *) "category = (?) "}, {(char *) "publisher = (?)"},
-        {(char *) "booktitle = (?)"}, {(char *) "bookreleaseyear = (?)"}, {(char *) "bookcover = (?)"},
-        {(char *) "hits = (?)"}
+        {(char *) "serialnum = (?)"}, {(char *) "type = (?) "}, {(char *) "category = (?) "},
+        {(char *) "publisher = (?)"}, {(char *) "booktitle = (?)"}, {(char *) "bookreleaseyear = (?)"},
+        {(char *) "bookcover = (?)"}, {(char *) "hits = (?)"}
     },
     {{(char *) "serialnum = (?) "}, {(char *) "lang = (?) "}},
     {{(char *) "serialnum = (?) "}, {(char *) "author = (?) "}},
@@ -163,6 +185,44 @@ static struct sqlbox_pstmt pstmts_bottom[STMTS__MAX - 1] = {
     {(char *) "WHERE UUID = (?) AND serialnum = (?) "}
 };
 
+enum khttp sanitize() {
+    if (r.method != KMETHOD_PUT)
+        return KHTTP_405;
+    if (r.page == PG__MAX)
+        return KHTTP_404;
+    return KHTTP_200;
+}
+
+enum statement get_stmts() {
+    enum key i;
+    for (i = KEY_PG_PUBLISHER; i < KEY_NAME && !(r.fieldmap[i]); i++) {
+    }
+    return i;
+}
+
 int main() {
+    enum khttp er;
+    // Parse the http request and match the keys to the keys, and pages to the pages, default to
+    // querying the INVENTORY if no page was found
+    if (khttp_parse(&r, keys, KEY__MAX, pages, PG__MAX, PG__MAX) != KCGI_OK)
+        return EXIT_FAILURE;
+
+    if ((er = sanitize()) != KHTTP_200) {
+        khttp_head(&r, kresps[KRESP_STATUS], "%s", khttps[er]);
+        khttp_head(&r, kresps[KRESP_ACCESS_CONTROL_ALLOW_ORIGIN], "%s", "*");
+        khttp_head(&r, kresps[KRESP_VARY], "%s", "Origin");
+        khttp_body(&r);
+        if (r.mime == KMIME_TEXT_HTML)
+            khttp_puts(&r, "Could not service request.");
+        khttp_free(&r);
+        return 0;
+    }
+    const enum statement STMT = get_stmts();
+    khttp_head(&r, kresps[KRESP_STATUS], "%s", khttps[KHTTP_200]);
+    khttp_head(&r, kresps[KRESP_ACCESS_CONTROL_ALLOW_ORIGIN], "%s", "*");
+    khttp_head(&r, kresps[KRESP_VARY], "%s", "Origin");
+    khttp_body(&r);
+    khttp_puts(&r, pstmts_top[STMT].stmt);
+    khttp_free(&r);
     return 0;
 }
