c22a0d908aae7b3353d4dead787876e4f13cfd27
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2025-04-07 21:13:14 +0100

    Initial structure

--- src/edit.c
+++ src/edit.c
@@ -18,20 +18,129 @@
 struct kreq r;
 struct kjsonreq req;
 
+
+enum pg {
+    PG_PUBLISHER,
+    PG_AUTHOR,
+    PG_LANG,
+    PG_ACTION,
+    PG_DOCTYPE,
+    PG_CAMPUS,
+    PG_ROLE,
+    PG_CATEGORY,
+    PG_ACCOUNT,
+    PG_BOOK,
+    PG_STOCK,
+    PG_INVENTORY,
+    PG__MAX
+};
+
+
+static const char *pages[PG__MAX] = {
+    "publisher", "author", "lang", "action", "doctype", "campus", "role", "category", "account", "book", "stock",
+    "inventory"
+};
+
+enum key {
+    KEY_PG_PUBLISHER,
+    KEY_PG_AUTHOR,
+    KEY_PG_LANG,
+    KEY_PG_ACTION,
+    KEY_PG_DOCTYPE,
+    KEY_PG_CAMPUS,
+    KEY_PG_ROLE,
+    KEY_PG_CATEGORY,
+    KEY_PG_ACCOUNT,
+    KEY_PG_BOOK,
+    KEY_PG_STOCK,
+    KEY_PG_INVENTORY,
+    KEY_PG_HISTORY,
+    KEY_PG_SESSIONS,
+    KEY__MAX
+};
+
+static const struct kvalid keys[KEY__MAX] = {
+    {NULL, "publisher"},
+    {NULL, "author"},
+    {NULL, "lang"},
+    {NULL, "action"},
+    {NULL, "doctype"},
+    {NULL, "campus"},
+    {NULL, "role"},
+    {NULL, "category"},
+    {NULL, "account"},
+    {NULL, "book"},
+    {NULL, "stock"},
+    {NULL, "inventory"},
+};
+
 enum statement {
-    STMTS_LOGOUT,
-    STMTS_LOGOUTALL,
-    STMTS_CUSTOM,
+    STMTS_PUBLISHER,
+    STMTS_AUTHOR,
+    STMTS_LANG,
+    STMTS_ACTION,
+    STMTS_TYPE,
+    STMTS_CAMPUS,
+    STMTS_ROLE,
+    STMTS_CATEGORY,
+    STMTS_ACCOUNT,
+    STMTS_BOOK,
+    STMTS_STOCK,
+    STMTS_INVENTORY,
+    __STMT_STORE__,
     STMTS__MAX
 };
 
-static struct sqlbox_pstmt pstmts[STMTS__MAX] = {
-    {(char *) "DELETE FROM SESSIONS WHERE sessionID = (?)"},
-    {(char *) "DELETE FROM SESSIONS WHERE account = (?)"},
-    {(char *) "DELETE FROM SESSIONS WHERE IIF((?) = 'ACCOUNT', account,sessionID) = (?)" }
+static const char *statement_string[STMTS__MAX] = {
+    "publisher", "author", "lang", "action", "type", "campus", "role", "category", "account",
+    "book", "stock", "inventory"
 };
 
-int main(){
+static struct sqlbox_pstmt pstmts_top[STMTS__MAX] = {
+    {(char *) "UPDATE PUBLISHER SET "},
+    {(char *) "UPDATE AUTHOR SET "},
+    {(char *) "UPDATE ACTION SET "},
+    {(char *) "UPDATE LANG SET "},
+    {(char *) "UPDATE DOCTYPE SET "},
+    {(char *) "UPDATE CAMPUS SET "},
+    {(char *) "UPDATE ROLE SET "},
+    {(char *) "UPDATE CATEGORY SET "},
+    {(char *) "UPDATE ACCOUNT SET "},
+    {(char *) "UPDATE BOOK SET "},
+};
 
-  return 0;
+static struct sqlbox_pstmt pstmts_middleware[STMTS__MAX][7] = {
+    {{(char *) "publisherName = (?) "}},
+    {{(char *) "authorName = (?) "}},
+    {{(char *) "actionName = (?) "}},
+    {{(char *) "langcode = (?) "}},
+    {{(char *) "typeName = (?) "}},
+    {{(char *) "campusName = (?) "}},
+    {{(char *) "roleName = (?) "}, {(char *) "perms = (?) "}},
+    {{(char *) "categoryClass = (?) "}, {(char *) "categoryName = (?) "}, {(char *) "parentCategoryID = (?) "}},
+    {
+        {(char *) "UUID = (?) "}, {(char *) "displayname = (?) "}, {(char *) "pwhash = (?) "},
+        {(char *) "campus = (?) "}, {(char *) "role = (?) "}, {(char *) "frozen = (?) "}
+    },
+    {
+        {(char *) "type = (?) "}, {(char *) "category = (?) "}, {(char *) "publisher = (?)"},
+        {(char *) "booktitle = (?)"}, {(char *) "bookreleaseyear = (?)"}, {(char *) "bookcover = (?)"},
+        {(char *) "hits = (?)"}
+    },
+};
+static struct sqlbox_pstmt pstmts_bottom[STMTS__MAX] = {
+    {(char *) "WHERE publisherName = (?)"},
+    {(char *) "WHERE authorName = (?)"},
+    {(char *) "WHERE actionName = (?)"},
+    {(char *) "WHERE langcode = (?) "},
+    {(char *) "WHERE typeName = (?) "},
+    {(char *) "WHERE campusName = (?) "},
+    {(char *) "WHERE roleName = (?) "},
+    {(char *) "WHERE categoryClass = (?) "},
+    {(char *) "WHERE UUID = (?) "},
+    {(char *) "WHERE serialnum = (?) "},
+};
+
+int main() {
+    return 0;
 }
