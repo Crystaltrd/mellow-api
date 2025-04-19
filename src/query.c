8bdef3523c229ef6d733641fb02f08d61e7bd3ba
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2025-04-19 14:49:22 +0100

    Add cascade to params

--- src/query.c
+++ src/query.c
@@ -1013,10 +1013,11 @@ void fill_params(const enum statement STATEMENT) {
 
             parms[0] = (struct sqlbox_parm){
                 .type = SQLBOX_PARM_STRING,
-                .sparm = ((r.fieldmap[KEY_FILTER_TREE] || r.fieldmap[KEY_FILTER_CASCADE]) && (!((field = r.fieldmap[KEY_FILTER_BY_PARENT])) || field->valsz
+                /*.sparm = (r.fieldmap[KEY_FILTER_TREE] && (!((field = r.fieldmap[KEY_FILTER_BY_PARENT])) || field->valsz
                                                           <= 0)) || r.fieldmap
                              ? "ROOT"
-                             : "NOT_ROOT"
+                             : "NOT_ROOT"*/
+                .sparm = (r.fieldmap[KEY_FILTER_CASCADE] && !r.fieldmap[KEY_FILTER_BY_PARENT]) ? "ROOT" : "NOT_ROOT"
             };
             parms[1] = (struct sqlbox_parm){
                 .type = SQLBOX_PARM_STRING,
