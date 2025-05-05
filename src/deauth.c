bc156467c42c5efe313300dab8cd321b2b09dd55
AIT MEDDOUR Fouad-Eddine <154087113+crystaltrd@users.noreply.github.com>
2025-05-05 23:13:50 +0100

    Add default cors for local
    Signed-off-by: Lain <crystaltrd@riseup.net>

--- src/deauth.c
+++ src/deauth.c
@@ -247,7 +247,7 @@ int main() {
     if (khttp_parse(&r, keys, KEY__MAX, 0, 0, 0) != KCGI_OK)
         return EXIT_FAILURE;
 
-    khttp_head(&r, "Access-Control-Allow-Origin", "https://seele.serveo.net");
+    khttp_head(&r, "Access-Control-Allow-Origin", "https://seele.serveo.net, http://localhost:5179");
     khttp_head(&r, "Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     khttp_head(&r, "Access-Control-Allow-Credentials", "true");
     if ((er = sanitize()) != KHTTP_200) goto error;
