5d8defa6338f330f00f9308d04d0f1184d70dc0f
Lain <crystaltrd@riseup.net>
2025-05-11 18:26:41 +0100

    Add me
    Signed-off-by: Lain <crystaltrd@riseup.net>

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ DESTDIR=/var/www/cgi-bin
 USER=www
 GROUP=www
 
-all: build/add build/edit build/query build/auth build/deauth build/signup build/search build/database.db
+all: build/add build/edit build/query build/auth build/deauth build/signup build/search build/database.db build/me
 install:install-edit install-add install-auth install-deauth install-query install-signup install-search install-db
 
 build/add.o: src/add.c
@@ -14,20 +14,22 @@ build/add: build/add.o
 	${CC} -o build/add build/add.o ${LDFLAGS} ${LDFLAGS_LINUX}
 install-add: build/add
 	install -o ${USER} -g ${GROUP} -m 0500 build/add ${DESTDIR}/add
-clean-add: build/add.o build/add
-	rm build/add
-	rm build/add.o
+
 
 build/auth.o: src/auth.c
 	${CC} ${CFLAGS} -c -o build/auth.o src/auth.c
 build/auth: build/auth.o
 	${CC} -o build/auth build/auth.o ${LDFLAGS} ${LDFLAGS_LINUX}
-
 install-auth: build/auth
 	install -o ${USER} -g ${GROUP} -m 0500 build/auth ${DESTDIR}/auth
-clean-auth: build/auth.o build/auth
-	rm build/auth
-	rm build/auth.o
+
+
+build/borrow.o: src/borrow.c
+	${CC} ${CFLAGS} -c -o build/borrow.o src/borrow.c
+build/borrow: build/borrow.o
+	${CC} -o build/borrow build/borrow.o ${LDFLAGS} ${LDFLAGS_LINUX}
+install-borrow: build/borrow
+	install -o ${USER} -g ${GROUP} -m 0500 build/borrow ${DESTDIR}/borrow
 
 
 build/deauth.o: src/deauth.c
@@ -36,9 +38,15 @@ build/deauth: build/deauth.o
 	${CC} -o build/deauth build/deauth.o ${LDFLAGS} ${LDFLAGS_LINUX}
 install-deauth: build/deauth
 	install -o ${USER} -g ${GROUP} -m 0500 build/deauth ${DESTDIR}/deauth
-clean-deauth: build/deauth.o build/deauth
-	rm build/deauth
-	rm build/deauth.o
+
+
+build/delete.o: src/delete.c
+	${CC} ${CFLAGS} -c -o build/delete.o src/delete.c
+build/delete: build/delete.o
+	${CC} -o build/delete build/delete.o ${LDFLAGS} ${LDFLAGS_LINUX}
+install-delete: build/delete
+	install -o ${USER} -g ${GROUP} -m 0500 build/delete ${DESTDIR}/delete
+
 
 build/edit.o: src/edit.c
 	${CC} ${CFLAGS} -c -o build/edit.o src/edit.c
@@ -46,9 +54,23 @@ build/edit: build/edit.o
 	${CC} -o build/edit build/edit.o ${LDFLAGS} ${LDFLAGS_LINUX}
 install-edit: build/edit
 	install -o ${USER} -g ${GROUP} -m 0500 build/edit ${DESTDIR}/edit
-clean-edit: build/edit.o build/edit
-	rm build/edit
-	rm build/edit.o
+
+
+build/me.o: src/me.c
+	${CC} ${CFLAGS} -c -o build/me.o src/me.c
+build/me: build/me.o
+	${CC} -o build/me build/me.o ${LDFLAGS} ${LDFLAGS_LINUX}
+install-me: build/me
+	install -o ${USER} -g ${GROUP} -m 0500 build/me ${DESTDIR}/me
+
+
+build/hit.o: src/hit.c
+	${CC} ${CFLAGS} -c -o build/hit.o src/hit.c
+build/hit: build/hit.o
+	${CC} -o build/hit build/hit.o ${LDFLAGS} ${LDFLAGS_LINUX}
+install-hit: build/hit
+	install -o ${USER} -g ${GROUP} -m 0500 build/hit ${DESTDIR}/hit
+
 
 build/query.o: src/query.c
 	${CC} ${CFLAGS} -c -o build/query.o src/query.c
@@ -56,9 +78,15 @@ build/query: build/query.o
 	${CC} -o build/query build/query.o ${LDFLAGS} ${LDFLAGS_LINUX}
 install-query: build/query
 	install -o ${USER} -g ${GROUP} -m 0500 build/query ${DESTDIR}/query
-clean-query: build/query.o build/query
-	rm build/query
-	rm build/query.o
+
+
+build/return.o: src/return.c
+	${CC} ${CFLAGS} -c -o build/return.o src/return.c
+build/return: build/return.o
+	${CC} -o build/return build/return.o ${LDFLAGS} ${LDFLAGS_LINUX}
+install-return: build/return
+	install -o ${USER} -g ${GROUP} -m 0500 build/return ${DESTDIR}/return
+
 
 build/signup.o: src/signup.c
 	${CC} ${CFLAGS} -c -o build/signup.o src/signup.c
@@ -66,9 +94,7 @@ build/signup: build/signup.o
 	${CC} -o build/signup build/signup.o ${LDFLAGS} ${LDFLAGS_LINUX}
 install-signup: build/signup
 	install -o ${USER} -g ${GROUP} -m 0500 build/signup ${DESTDIR}/signup
-clean-signup: build/signup.o build/signup
-	rm build/signup
-	rm build/signup.o
+
 
 build/search.o: src/search.c
 	${CC} ${CFLAGS} -c -o build/search.o src/search.c
@@ -76,9 +102,7 @@ build/search: build/search.o
 	${CC} -o build/search build/search.o ${LDFLAGS} ${LDFLAGS_LINUX}
 install-search: build/search
 	install -o ${USER} -g ${GROUP} -m 0500 build/search ${DESTDIR}/search
-clean-search: build/search.o build/search
-	rm build/search
-	rm build/search.o
+
 
 build/database.db: misc/database-scheme.sql
 	[ -f build/database.db ] && rm build/database.db || echo "Skipping db"
@@ -88,7 +112,7 @@ install-db: build/database.db
 	chown ${USER}:${GROUP} ${DESTDIR}/db
 	chmod 0700 ${DESTDIR}/db
 	install -o ${USER} -g ${GROUP} -m 0600 build/database.db ${DESTDIR}/db
-clean-db: build/database.db
-	rm build/database.db
 
-clean: clean-auth clean-deauth clean-db clean-edit clean-query clean-signup clean-search
+
+clean:
+	rm -rfv build/*
---
+++ src/me.c
@@ -0,0 +1,209 @@
+#include <sys/types.h> /* size_t, ssize_t */
+#include <stdarg.h> /* va_list */
+#include <stddef.h> /* NULL */
+#include <stdint.h> /* int64_t */
+#include <unistd.h> /* pledge() */
+#include <err.h> /* err(), warnx() */
+#include <inttypes.h>
+#include <stdlib.h> /* EXIT_FAILURE */
+#include <string.h> /* memset() */
+#include <kcgi.h>
+#include <kcgijson.h>
+#include <sqlbox.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+struct kreq r;
+struct kjsonreq req;
+/*
+ * The Permissions struct for a role, the order in this struct is the same as
+ * how it should be interpreted in binary
+ */
+struct accperms {
+    int numeric;
+    bool admin;
+    bool staff;
+    bool manage_stock;
+    bool manage_inventories;
+    bool see_accounts;
+    bool monitor_history;
+    bool has_inventory;
+};
+
+struct accperms int_to_accperms(int perm) {
+    struct accperms perms = {
+        .numeric = perm,
+        .admin = (perm & (1 << 6)),
+        .staff = (perm & (1 << 5)),
+        .manage_stock = (perm & (1 << 4)),
+        .manage_inventories = (perm & (1 << 3)),
+        .see_accounts = (perm & (1 << 2)),
+        .monitor_history = (perm & (1 << 1)),
+        .has_inventory = (perm & 1)
+    };
+    return perms;
+}
+
+
+enum key {
+    COOKIE_SESSIONID,
+    KEY__MAX
+};
+
+static const struct kvalid keys[KEY__MAX] = {
+    {kvalid_stringne, "sessionID"},
+};
+enum statment {
+    STMTS_LOGIN,
+    STMTS__MAX
+};
+
+static struct sqlbox_pstmt pstmts[STMTS__MAX] = {
+    {
+        (char *)
+        "SELECT ACCOUNT.UUID, displayname, pwhash, campus, role, perms, frozen "
+        "FROM ROLE,"
+        "ACCOUNT "
+        "LEFT JOIN SESSIONS S on ACCOUNT.UUID = S.account "
+        "WHERE ACCOUNT.role = ROLE.roleName "
+        "AND sessionID = (?) "
+        "GROUP BY ACCOUNT.UUID, displayname, pwhash, campus, perms, frozen "
+    }
+};
+
+struct sqlbox_src srcs[] = {
+    {
+        .fname = (char *) "db/database.db",
+        .mode = SQLBOX_SRC_RW
+    }
+};
+struct sqlbox *boxctx;
+struct sqlbox_cfg cfg;
+size_t dbid; // Database associated with a config and a context for query results
+
+/*
+ * Allocates the context and source for the current operations
+ */
+void alloc_ctx_cfg() {
+    memset(&cfg, 0, sizeof(struct sqlbox_cfg));
+    cfg.msg.func_short = warnx;
+    cfg.srcs.srcsz = 1;
+    cfg.srcs.srcs = srcs;
+    cfg.stmts.stmtsz = STMTS__MAX;
+    cfg.stmts.stmts = pstmts;
+    if ((boxctx = sqlbox_alloc(&cfg)) == NULL)
+        errx(EXIT_FAILURE, "sqlbox_alloc");
+    if (!(dbid = sqlbox_open(boxctx, 0)))
+        errx(EXIT_FAILURE, "sqlbox_open");
+}
+
+struct usr {
+    char *UUID;
+    char *disp_name;
+    char *campus;
+    char *role;
+    char *sessionID;
+    struct accperms perms;
+    bool authenticated;
+    bool frozen;
+};
+
+struct usr curr_usr = {
+    .authenticated = false,
+    .frozen = false,
+    .UUID = NULL,
+    .disp_name = NULL,
+    .campus = NULL,
+    .role = NULL,
+    .sessionID = NULL,
+    .perms = {0, 0, 0, 0, 0, 0, 0, 0}
+};
+
+void fill_user() {
+    struct kpair *field;
+
+    if ((field = r.cookiemap[COOKIE_SESSIONID])) {
+        size_t stmtid;
+        size_t parmsz = 1;
+        const struct sqlbox_parmset *res;
+        struct sqlbox_parm parms[] = {
+            {.type = SQLBOX_PARM_STRING, .sparm = field->parsed.s},
+        };
+        if (!(stmtid = sqlbox_prepare_bind(boxctx, dbid, STMTS_LOGIN, parmsz, parms, 0)))
+            errx(EXIT_FAILURE, "sqlbox_prepare_bind");
+        if ((res = sqlbox_step(boxctx, stmtid)) == NULL)
+            errx(EXIT_FAILURE, "sqlbox_step");
+        if (res->psz != 0) {
+            curr_usr.authenticated = true;
+            kasprintf(&curr_usr.UUID, "%s", res->ps[0].sparm);
+
+            kasprintf(&curr_usr.disp_name, "%s", res->ps[1].sparm);
+
+            kasprintf(&curr_usr.campus, "%s", res->ps[3].sparm);
+
+            kasprintf(&curr_usr.role, "%s", res->ps[4].sparm);
+
+            kasprintf(&curr_usr.sessionID, "%s", field->parsed.s);
+
+            curr_usr.perms = int_to_accperms((int) res->ps[5].iparm);
+
+            curr_usr.frozen = res->ps[6].iparm;
+        }
+        sqlbox_finalise(boxctx, stmtid);
+    }
+}
+enum khttp sanitize() {
+    if (r.method != KMETHOD_GET)
+        return KHTTP_405;
+    return KHTTP_200;
+}
+int main() {
+    enum khttp er;
+    if (khttp_parse(&r, keys, KEY__MAX, 0, 0, 0) != KCGI_OK)
+        return EXIT_FAILURE;
+    khttp_head(&r, "Access-Control-Allow-Origin", "https://seele.serveo.net");
+    khttp_head(&r, "Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    khttp_head(&r, "Access-Control-Allow-Credentials", "true");
+    if ((er = sanitize()) != KHTTP_200) {
+        khttp_head(&r, kresps[KRESP_STATUS], "%s", khttps[er]);
+        khttp_body(&r);
+        if (r.mime == KMIME_TEXT_HTML)
+            khttp_puts(&r, "Could not service request.");
+        khttp_free(&r);
+        return 0;
+    }
+    alloc_ctx_cfg();
+    fill_user();
+    khttp_head(&r, kresps[KRESP_STATUS], "%s", khttps[KHTTP_200]);
+    khttp_head(&r, kresps[KRESP_CONTENT_TYPE], "%s", kmimetypes[KMIME_APP_JSON]);
+    khttp_body(&r);
+    kjson_open(&req, &r);
+    kjson_obj_open(&req);
+    kjson_objp_open(&req, "user");
+    kjson_putstringp(&req, "IP", r.remote);
+    kjson_putboolp(&req, "authenticated", curr_usr.authenticated);
+    if (curr_usr.authenticated) {
+        kjson_putstringp(&req, "UUID", curr_usr.UUID);
+        kjson_putstringp(&req, "disp_name", curr_usr.disp_name);
+        kjson_putstringp(&req, "campus", curr_usr.campus);
+        kjson_putstringp(&req, "role", curr_usr.role);
+        kjson_putboolp(&req, "frozen", curr_usr.frozen);
+
+        kjson_objp_open(&req, "perms");
+        kjson_putintp(&req, "numeric", curr_usr.perms.numeric);
+        kjson_putboolp(&req, "admin", curr_usr.perms.admin);
+        kjson_putboolp(&req, "staff", curr_usr.perms.staff);
+        kjson_putboolp(&req, "manage_stock", curr_usr.perms.manage_stock);
+        kjson_putboolp(&req, "manage_inventories", curr_usr.perms.manage_inventories);
+        kjson_putboolp(&req, "see_accounts", curr_usr.perms.see_accounts);
+        kjson_putboolp(&req, "monitor_history", curr_usr.perms.monitor_history);
+        kjson_putboolp(&req, "has_inventory", curr_usr.perms.has_inventory);
+        kjson_obj_close(&req);
+    }
+    kjson_obj_close(&req);
+    kjson_obj_close(&req);
+    kjson_close(&req);
+    khttp_free(&r);
+    sqlbox_free(boxctx);
+    return EXIT_SUCCESS;
+}
